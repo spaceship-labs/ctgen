005738fbf553c2c7c2c354a177fe5c6478c1ded6
urielaero <aero.uriel@gmail.com>
2015-09-07 11:06:24 -0500

    sources

--- test/sources.js
+++ test/sources.js
@@ -1,8 +1,9 @@
-var fs = require('fs'),
+var fs = require('fs-extra'),
     should = require('should'),
     mockery = require('mockery'),
     sinon = require('sinon'),
     request = require('request'),
+    unzip = require('node-unzip-2'),
     htmlContratos = fs.readFileSync('test/html-stubs/contrataciones.html', 'utf-8');
 
 describe('sources', function(){
@@ -12,6 +13,23 @@ describe('sources', function(){
         url: 'https://sites.google.com/site/cnetuc/contrataciones',
         parent: '.sites-layout-tile table ',
         each: 'tr td a'
+      },
+      streamPipe = {
+        pipe: function(dest){//simulate streams
+          dest.run('finish')
+        }
+      },
+      streamResponse = function(name){
+        var events = {};
+        return {
+          on: function(event, done){
+            events[event] = done;
+          },
+          run: function(event){
+            events[event] && events[event]();
+          },
+          pipe: streamPipe.pipe
+        }
       };
 
   before(function(){
@@ -21,13 +39,23 @@ describe('sources', function(){
       useCleanCache: true
     });
 
+    stubs.fs = {};
+
+    stubs.fs.createOutputStream = sinon.stub(fs, 'createOutputStream', streamResponse);
+    stubs.fs.createReadStream = sinon.stub(fs, 'createReadStream', streamResponse);
+
     stubs.request = sinon.stub(request, 'get');
     stubs.request.withArgs({url: infoContratos.url}).yieldsAsync(null, {}, htmlContratos);
+    stubs.request.withArgs('http://upcp.funcionpublica.gob.mx/descargas/Contratos2010_2012.zip').returns(streamPipe);
 
+    stubs.unzip = sinon.stub(unzip, 'Extract');
+    stubs.unzip.withArgs({path: 'unzip/'+'downloadtestupcpfuncionpublicagobmxdescargasContratos2010_2012'}).returns(streamResponse());
+
+    mockery.registerMock('fs-extra', fs);
     mockery.registerMock('request', request);
+    mockery.registerMock('node-unzip-2', unzip);
 
     sources = require('../lib/sources');
-
   });
 
   describe('getDownloadLink', function(){
@@ -58,12 +86,41 @@ describe('sources', function(){
     });
   });
 
-  /*
   describe('downloadFiles', function(){
-    it('should save the files in path', function(done){
-
+    it('should download files', function(done){
+      sources.downloadFiles('http://upcp.funcionpublica.gob.mx/descargas/Contratos2010_2012.zip', 'downloadtest/', function(err, data){
+        data.should.be.eql(['downloadtest/upcpfuncionpublicagobmxdescargasContratos2010_2012.zip']);
+        done();
+      });
     });
   });
-  */
 
+  describe('unZipFiles', function(){
+
+    beforeEach(function(){
+      stubs.unzip.reset();
+      stubs.fs.createReadStream.reset();
+    });
+
+    it('should unzip files', function(done){
+      sources.unZipFiles('downloadtest/upcpfuncionpublicagobmxdescargasContratos2010_2012.zip', 'unzip/', function(err, data){
+        data.should.be.eql([ 'unzip/downloadtestupcpfuncionpublicagobmxdescargasContratos2010_2012' ])
+        done();
+      })
+    });
+
+    it('should work with .zip extension', function(done){
+      sources.unZipFiles('downloadtest/upcpfuncionpublicagobmxdescargasContratos2010_2012.csv', 'unzip/', function(err, data){
+        stubs.fs.createReadStream.called.should.be.equal(false);
+        stubs.unzip.called.should.be.equal(false);
+        data.should.be.eql(['downloadtest/upcpfuncionpublicagobmxdescargasContratos2010_2012.csv'])
+        done();
+      })
+    });
+  });
+
+  after(function(){
+    mockery.deregisterAll();
+    mockery.disable();
+  });
 });
