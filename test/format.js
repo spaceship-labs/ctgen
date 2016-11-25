13a64a0084098ffd5877c7f4e4f88585577f4213
urielaero <aero.uriel@gmail.com>
2016-11-25 00:15:53 -0600

    factory test for format

--- test/format.js
+++ test/format.js
@@ -1,7 +1,6 @@
 var should = require('should'),
     mockery = require('mockery'),
     fs = require('fs-extra'),
-    xls = require('pyspreadsheet').SpreadsheetReader,
     sinon = require('sinon');
 
 describe('format', function(){
@@ -26,7 +25,8 @@ describe('format', function(){
 
     //testfile.xls
     var testfile = {
-      sheets: [{
+      sheet: {
+        index: 0,
         name: 'hoja1',
         rows: [
           [{address:'a0', value: null},
@@ -49,29 +49,103 @@ describe('format', function(){
           {address: 'b4',},
           {address: 'c4',value: 'field4.2 \na'}]
         ]
-      }]
+      }
+    };
+
+    var testfileNewFields = {
+      sheet: {
+        index: 0,
+        name: 'hoja1',
+
+        },
+        rows: testfile.sheet.rows
+        /*
+        rows: [
+          [{address:'a0', value: null},
+          {address:'b0', value: null },
+          {address:'c0', value: null },
+          {address:'d0', value: null }],
+          [{address: 'a1', value: 'nameField1'},
+          {address: 'b1', value: 'new field'},
+          {address: 'c1', value: 'nameField2'},
+          {address: 'd1', value: 'nameField3'}],
+          [{address: 'a2', value: 'field1'},
+          {address: 'b2', value: 'newfield1'},
+          {address: 'c2', value: 'field2'},
+          {address: 'd2', value: 'field3'}],
+          [{address: 'a3'},
+          {address: 'b3',value: 'newfield2.1'},
+          {address: 'c3',value: 'field2.1'},
+          {address: 'd3',value: 'field3.1'}],
+          [{address: 'a4',},
+          {address: 'b4',},
+          {address: 'c4',},
+          {address: 'd4',value: 'field3.2 with comm"'}],
+          [{address: 'a5',},
+          {address: 'b5',},
+          {address: 'c5',},
+          {address: 'd5',value: 'field4.2 \na'}]
+        ]
+        */
+
     };
 
     stubs.fs = {};
-    stubs.xls = sinon.stub(xls, 'read');
+    stubs.xlsObj = function () {
+      var self = this;
+      self.events = {};
+      self.on = function(event, fn) {
+        self.events[event] = fn;
+        if (event == 'data') {
+          fn(testfileNewFields);
+        } else if (event == 'close') {
+          fn();
+
+        }
+        //console.log('asddddddddd-------------');
+        return self;
+      };
+
+    };
+
+    stubs.xlsObj.read = function() {};
+    stubs.xls = sinon.stub(stubs.xlsObj, 'read');
     stubs.fs.createOutputStream = sinon.stub(fs, 'createOutputStream');
+    /*
+    stubs.xlsObj.returns({
+      end: function () {
+        stubs.xlsObj.events.close();
+      },
+      on: function (ev, done) {
+        console.log('run!')
+        stubs.xlsObj.events[ev] = done;
+      },
+    });
+    */
     stubs.xls.withArgs(sinon.match.string).yieldsAsync(null, testfile);
 
-    stubs.fs.createOutputStreamWrite = sinon.spy();
+    stubs.fs.createOutputStreamWriteRaw = '';
+    stubs.fs.createOutputStreamWrite = sinon.spy(function(text) {
+      stubs.fs.createOutputStreamWriteRaw += text;
+    });
     stubs.fs.createOutputStreamEnd = sinon.spy();
     stubs.fs.events = {};
-    stubs.fs.createOutputStream.withArgs('csv/output.csv').returns({
+
+    var returnsForCreateOutputStream = {
       write: stubs.fs.createOutputStreamWrite,
       end: function(){
         stubs.fs.createOutputStreamEnd();
-        stubs.fs.events.finish();
+        if (stubs.fs.events.finish)
+          stubs.fs.events.finish();
       },
       on: function(ev, done){
         stubs.fs.events[ev] = done;
       }
-    })
+    };
 
-    mockery.registerMock('pyspreadsheet', {SpreadsheetReader: xls});
+    stubs.fs.createOutputStream.withArgs('csv/output.csv').returns(returnsForCreateOutputStream);
+
+    mockery.registerMock('pyspreadsheet', {SpreadsheetReader: stubs.xlsObj});
     mockery.registerMock('fs-extra', fs);
 
     format = require('../lib/format');
@@ -81,11 +155,12 @@ describe('format', function(){
     stubs.fs.createOutputStream.reset();
     stubs.fs.createOutputStreamWrite.reset();
     stubs.fs.createOutputStreamEnd.reset();
+    stubs.fs.createOutputStreamWriteRaw = '';
   });
 
   describe('xlsx2csv', function(){
     it('should format csv', function(done){
-      format.xls2csv('testfile.xls', function(err, csv){
+      format.xls2csv_stream('testfile.xls', function(err, csv){
         stubs.fs.createOutputStream.called.should.be.equal(false);
         csv.should.be.equal(expectCsv);
         done();
@@ -98,15 +173,28 @@ describe('format', function(){
               ',"field2.1","field3.1"\n' +
               ',,"field3.2 with comm\'"\n' +
               ',,"field4.2 a"\n';
-      format.xls2csv('testfile.xls', {headCsv: '"namefieldhead1","namefieldhead2","namefieldhead3"'}, function(err, csv){
+      format.xls2csv_stream('testfile.xls', {headCsv: '"namefieldhead1","namefieldhead2","namefieldhead3"'}, function(err, csv){
         stubs.fs.createOutputStream.called.should.be.equal(false);
         csv.should.be.equal(expectCsvHead);
         done();
       });
     });
 
-    it('should format csv, if nameFile is nefined save with streams', function(done){
-      format.xls2csv('testfile.xls', {fileName: 'csv/output.csv'}, function(err, csv){
+    it('should format csv, used alias for head instead replace', function(done) {
+      var expectCsvHead = '"namefieldhead1","namefieldhead2","namefieldhead3"\n' +
+              '"field1","field2","field3"\n'+
+              ',"field2.1","field3.1"\n' +
+              ',,"field3.2 with comm\'"\n' +
+              ',,"field4.2 a"\n';
+      format.xls2csv_stream('testfile.xls', {headCsv: '"namefieldhead1","namefieldhead2","namefieldhead3"', HeadCsvAsAlias: true}, function(err, csv){
+        stubs.fs.createOutputStream.called.should.be.equal(false);
+        csv.should.be.equal(expectCsvHead);
+        done();
+      });
+    });
+
+    it('should format csv, if exist nameFile save with streams', function(done){
+      format.xls2csv_stream('testfile.xls', {fileName: 'csv/output.csv'}, function(err, csv){
         stubs.fs.createOutputStream.called.should.be.equal(true);
         var lines = expectCsv.split('\n');
         stubs.fs.createOutputStreamWrite.getCall(0).args[0].should.be.equal(lines[0]+'\n');
@@ -114,7 +202,7 @@ describe('format', function(){
         stubs.fs.createOutputStreamWrite.getCall(2).args[0].should.be.equal(lines[2]+'\n');
         stubs.fs.createOutputStreamWrite.getCall(3).args[0].should.be.equal(lines[3]+'\n');
         stubs.fs.createOutputStreamEnd.calledOnce.should.be.equal(true);
-        csv.should.be.equal(expectCsv);
+        stubs.fs.createOutputStreamWriteRaw.should.be.equal(expectCsv);
         done();
       });
     });
