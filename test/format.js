bcebc5a6b47a800f8bb0371d7d4558b36fe8033a
urielaero <aero.uriel@gmail.com>
2016-11-30 22:59:05 -0500

    fixed format csv

--- test/format.js
+++ test/format.js
@@ -28,28 +28,28 @@ describe('format', function(){
       sheet: {
         index: 0,
         name: 'hoja1',
-        rows: [
-          [{address:'a0', value: null},
-          {address:'b0', value: null },
-          {address:'c0', value: null },
-          {address:'d0', value: null }],
-          [{address: 'a1', value: 'nameField1'},
-          {address: 'b1', value: 'nameField2'},
-          {address: 'c1', value: 'nameField3'}],
-          [{address: 'a2', value: 'field1'},
-          {address: 'b2', value: 'field2'},
-          {address: 'c2', value: 'field3'}],
-          [{address: 'a3'},
-          {address: 'b3',value: 'field2.1'},
-          {address: 'c3',value: 'field3.1'}],
-          [{address: 'a4',},
-          {address: 'b4',},
-          {address: 'c4',value: 'field3.2 with comm"'}],
-          [{address: 'a4',},
-          {address: 'b4',},
-          {address: 'c4',value: 'field4.2 \na'}]
-        ]
-      }
+        },
+      rows: [
+        [{address:'a0', value: null},
+        {address:'b0', value: null },
+        {address:'c0', value: null },
+        {address:'d0', value: null }],
+        [{address: 'a1', value: 'nameField1'},
+        {address: 'b1', value: 'nameField2'},
+        {address: 'c1', value: 'nameField3'}],
+        [{address: 'a2', value: 'field1'},
+        {address: 'b2', value: 'field2'},
+        {address: 'c2', value: 'field3'}],
+        [{address: 'a3'},
+        {address: 'b3',value: 'field2.1'},
+        {address: 'c3',value: 'field3.1'}],
+        [{address: 'a4',},
+        {address: 'b4',},
+        {address: 'c4',value: 'field3.2 with comm"'}],
+        [{address: 'a4',},
+        {address: 'b4',},
+        {address: 'c4',value: 'field4.2 \na'}]
+      ]
     };
 
     var testfileNewFields = {
@@ -58,46 +58,49 @@ describe('format', function(){
         name: 'hoja1',
 
         },
-        rows: testfile.sheet.rows
-        /*
-        rows: [
-          [{address:'a0', value: null},
-          {address:'b0', value: null },
-          {address:'c0', value: null },
-          {address:'d0', value: null }],
-          [{address: 'a1', value: 'nameField1'},
-          {address: 'b1', value: 'new field'},
-          {address: 'c1', value: 'nameField2'},
-          {address: 'd1', value: 'nameField3'}],
-          [{address: 'a2', value: 'field1'},
-          {address: 'b2', value: 'newfield1'},
-          {address: 'c2', value: 'field2'},
-          {address: 'd2', value: 'field3'}],
-          [{address: 'a3'},
-          {address: 'b3',value: 'newfield2.1'},
-          {address: 'c3',value: 'field2.1'},
-          {address: 'd3',value: 'field3.1'}],
-          [{address: 'a4',},
-          {address: 'b4',},
-          {address: 'c4',},
-          {address: 'd4',value: 'field3.2 with comm"'}],
-          [{address: 'a5',},
-          {address: 'b5',},
-          {address: 'c5',},
-          {address: 'd5',value: 'field4.2 \na'}]
-        ]
-        */
-
+      //rows: testfile.sheet.rows
+      rows: [
+        [{address:'a0', value: null},
+        {address:'b0', value: null },
+        {address:'c0', value: null },
+        {address:'d0', value: null }],
+        [{address: 'a1', value: 'nameField1'},
+        {address: 'b1', value: 'new field'},
+        {address: 'c1', value: 'nameField2'},
+        {address: 'd1', value: 'nameField3'}],
+        [{address: 'a2', value: 'Field1'},
+        {address: 'b2', value: 'NewField1'},
+        {address: 'c2', value: 'Field2'},
+        {address: 'd2', value: 'Field3'}],
+        [{address: 'a3'},
+        {address: 'b3',value: 'newfielD2.1'},
+        {address: 'c3',value: 'fielD2.1'},
+        {address: 'd3',value: 'fielD3.1'}],
+        [{address: 'a4',},
+        {address: 'b4',},
+        {address: 'c4',},
+        {address: 'd4',value: 'field3.2 with comm"'}],
+        [{address: 'a5',},
+        {address: 'b5',},
+        {address: 'c5',},
+        {address: 'd5',value: 'field4.2 \na'}]
+      ]
     };
 
     stubs.fs = {};
-    stubs.xlsObj = function () {
+    stubs.xlsObj = function (file) {
+      console.log('file', file);
       var self = this;
       self.events = {};
       self.on = function(event, fn) {
+        console.log('event!', event);
         self.events[event] = fn;
         if (event == 'data') {
-          fn(testfileNewFields);
+          if (file == 'testfile.xls') {
+            fn(testfile);
+          } else {
+            fn(testfileNewFields);
+          }
         } else if (event == 'close') {
           fn();
 
@@ -167,7 +170,7 @@ describe('format', function(){
       });
     });
 
-    it('should format csv, if head is nefined replace this', function(done){
+    it('should format csv, if head is defined replace this', function(done){
       var expectCsvHead = '"namefieldhead1","namefieldhead2","namefieldhead3"\n' +
               '"field1","field2","field3"\n'+
               ',"field2.1","field3.1"\n' +
@@ -180,14 +183,16 @@ describe('format', function(){
       });
     });
 
-    it('should format csv, used alias for head instead replace', function(done) {
-      var expectCsvHead = '"namefieldhead1","namefieldhead2","namefieldhead3"\n' +
-              '"field1","field2","field3"\n'+
-              ',"field2.1","field3.1"\n' +
-              ',,"field3.2 with comm\'"\n' +
-              ',,"field4.2 a"\n';
-      format.xls2csv_stream('testfile.xls', {headCsv: '"namefieldhead1","namefieldhead2","namefieldhead3"', HeadCsvAsAlias: true}, function(err, csv){
+    it('should format csv, use header for csv in lower case', function(done) {
+      var expectCsvHead = '"namefield1","new field","namefield2","namefield3"\n' +
+              '"Field1","NewField1","Field2","Field3"\n'+
+              ',"newfielD2.1","fielD2.1","fielD3.1"\n' +
+              ',,,"field3.2 with comm\'"\n' +
+              ',,,"field4.2 a"\n';
+
+      format.xls2csv_stream('testfileNewFields.xls', {headCsvLowerCase: true}, function(err, csv){
         stubs.fs.createOutputStream.called.should.be.equal(false);
+        console.log('return', csv);
         csv.should.be.equal(expectCsvHead);
         done();
       });
