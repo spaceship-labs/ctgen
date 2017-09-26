6b7c4ec354b95cb04ca5e35055b5a32d56505cde
Erick <erick@Erick-pc>
2017-09-26 16:12:02 -0500

    validar proveedor contratista

--- scripts/mongo/05.empresas.js
+++ scripts/mongo/05.empresas.js
@@ -18,85 +18,87 @@ var bulk_c = db.contrato.initializeUnorderedBulkOp();
 
 db.contrato.find({ }).forEach(function (doc) {
   //print(doc.proveedor_contratista);
-  nombre_sin_acentos = doc.proveedor_contratista.replace(preCompiledAccentsRegex, function(str,a,c,e,i,n,o,s,u,y,ae) { if(a) return 'a'; else if(c) return 'c'; else if(e) return 'e'; else if(i) return 'i'; else if(n) return 'n'; else if(o) return 'o'; else if(s) return 's'; else if(u) return 'u'; else if(y) return 'y'; else if(ae) return 'ae'; });
-  nombre = nombre_sin_acentos.toUpperCase();
-  nombre = nombre.replace(preCompiledComaRegex, "");
-  nombre = nombre.replace(preCompiledDotRegex, "");
-  nombre = nombre.replace(preCompiledSpaceRegex, "");
-  money = doc.importe_contrato;
-  if (isNaN(doc.importe_contrato)) {
-    money = parseFloat(doc.importe_contrato.replace(preCompiledMoneyRegex, ''));
-  }
-  if (doc.source == "https://sites.google.com/site/cnetuc/contratos_cnet_3") {
-    dateString = doc.fecha_inicio.match(preCompiledDateRegex);
-    date = new Date( dateString[3], dateString[2]-1, dateString[1] ).getFullYear();
-  } else {
-    date = new Date(doc.fecha_inicio).getFullYear();
-  }
-
-  if(hashmap[nombre] === undefined) {
-    id = new ObjectId();
-    obj = {proveedor_contratista: nombre_sin_acentos, _id: id, createdAt: today_date, updatedAt: today_date};
-    bulk.insert( obj );
-
-    bulk_c.find( { _id: doc._id } ).update( { $set: { fecha_inicio_year: date, origProvedorContratista: doc.proveedor_contratista, provedorContratista: id, importe_contrato: money, codigoContratoProcedimiento : doc.codigo_contrato + '-' + doc.numero_procedimiento, slugProvedorContratista : nombre } } );
-
-    hashmap[nombre] = id;
-    //hashmap_names[hashmap[nombre]] = doc.proveedor_contratista.toString();
-  } else {
-    bulk_c.find( { _id: doc._id } ).update( { $set: { fecha_inicio_year: date, origProvedorContratista: doc.proveedor_contratista, provedorContratista: hashmap[nombre], importe_contrato: money, codigoContratoProcedimiento : doc.codigo_contrato + '-' + doc.numero_procedimiento, slugProvedorContratista : nombre } } );
-    /*if(hashmap_names[hashmap[nombre]] === undefined) {
-      hashmap_names[hashmap[nombre]] = doc.proveedor_contratista.toString();
+  if( doc.proveedor_contratista ){
+    nombre_sin_acentos = doc.proveedor_contratista.replace(preCompiledAccentsRegex, function(str,a,c,e,i,n,o,s,u,y,ae) { if(a) return 'a'; else if(c) return 'c'; else if(e) return 'e'; else if(i) return 'i'; else if(n) return 'n'; else if(o) return 'o'; else if(s) return 's'; else if(u) return 'u'; else if(y) return 'y'; else if(ae) return 'ae'; });
+    nombre = nombre_sin_acentos.toUpperCase();
+    nombre = nombre.replace(preCompiledComaRegex, "");
+    nombre = nombre.replace(preCompiledDotRegex, "");
+    nombre = nombre.replace(preCompiledSpaceRegex, "");
+    money = doc.importe_contrato;
+    if (isNaN(doc.importe_contrato)) {
+      money = parseFloat(doc.importe_contrato.replace(preCompiledMoneyRegex, ''));
+    }
+    if (doc.source == "https://sites.google.com/site/cnetuc/contratos_cnet_3") {
+      dateString = doc.fecha_inicio.match(preCompiledDateRegex);
+      date = new Date( dateString[3], dateString[2]-1, dateString[1] ).getFullYear();
     } else {
-      value = hashmap_names[hashmap[nombre]];
-      if (!value.indexOf(doc.proveedor_contratista.toString()) > -1) {
-        value += "|" + doc.proveedor_contratista.toString();
-        hashmap_names[hashmap[nombre]] = value;
+      date = new Date(doc.fecha_inicio).getFullYear();
+    }
+
+    if(hashmap[nombre] === undefined) {
+      id = new ObjectId();
+      obj = {proveedor_contratista: nombre_sin_acentos, _id: id, createdAt: today_date, updatedAt: today_date};
+      bulk.insert( obj );
+
+      bulk_c.find( { _id: doc._id } ).update( { $set: { fecha_inicio_year: date, origProvedorContratista: doc.proveedor_contratista, provedorContratista: id, importe_contrato: money, codigoContratoProcedimiento : doc.codigo_contrato + '-' + doc.numero_procedimiento, slugProvedorContratista : nombre } } );
+
+      hashmap[nombre] = id;
+      //hashmap_names[hashmap[nombre]] = doc.proveedor_contratista.toString();
+    } else {
+      bulk_c.find( { _id: doc._id } ).update( { $set: { fecha_inicio_year: date, origProvedorContratista: doc.proveedor_contratista, provedorContratista: hashmap[nombre], importe_contrato: money, codigoContratoProcedimiento : doc.codigo_contrato + '-' + doc.numero_procedimiento, slugProvedorContratista : nombre } } );
+      /*if(hashmap_names[hashmap[nombre]] === undefined) {
+        hashmap_names[hashmap[nombre]] = doc.proveedor_contratista.toString();
+      } else {
+        value = hashmap_names[hashmap[nombre]];
+        if (!value.indexOf(doc.proveedor_contratista.toString()) > -1) {
+          value += "|" + doc.proveedor_contratista.toString();
+          hashmap_names[hashmap[nombre]] = value;
+        }
+      }*/
+    }
+
+    if(doc.moneda === undefined || doc.moneda.toUpperCase() == "MXN") {
+      if(importe_hashmap[hashmap[nombre]] === undefined) {
+        importe_hashmap[hashmap[nombre]] = parseFloat(doc.importe_contrato);
+      } else {
+        importe_hashmap[hashmap[nombre]] = parseFloat(importe_hashmap[hashmap[nombre]]) + parseFloat(doc.importe_contrato);
       }
-    }*/
-  }
-
-  if(doc.moneda === undefined || doc.moneda.toUpperCase() == "MXN") {
-    if(importe_hashmap[hashmap[nombre]] === undefined) {
-      importe_hashmap[hashmap[nombre]] = parseFloat(doc.importe_contrato);
     } else {
-      importe_hashmap[hashmap[nombre]] = parseFloat(importe_hashmap[hashmap[nombre]]) + parseFloat(doc.importe_contrato);
+      if(importe_hashmap_usd[hashmap[nombre]] === undefined) {
+        importe_hashmap_usd[hashmap[nombre]] = parseFloat(doc.importe_contrato);
+      } else {
+        importe_hashmap_usd[hashmap[nombre]] = parseFloat(importe_hashmap_usd[hashmap[nombre]]) + parseFloat(doc.importe_contrato);
+      }
     }
-  } else {
-    if(importe_hashmap_usd[hashmap[nombre]] === undefined) {
-      importe_hashmap_usd[hashmap[nombre]] = parseFloat(doc.importe_contrato);
-    } else {
-      importe_hashmap_usd[hashmap[nombre]] = parseFloat(importe_hashmap_usd[hashmap[nombre]]) + parseFloat(doc.importe_contrato);
+    counter++;
+    if(counter % 50000 === 0 ) {
+      print("ciclo " + counter);
     }
-  }
-  counter++;
-  if(counter % 50000 === 0 ) {
-    print("ciclo " + counter);
-  }
-  if(counter % 500 === 0 ) {
-    bulk.execute({ w: 0});
-    bulk = db.empresa.initializeUnorderedBulkOp();
-    bulk_c.execute({ w: 0});
-    bulk_c = db.contrato.initializeUnorderedBulkOp();
-  }
-});
-print("insertando " + Object.keys(hashmap).length + " registros");
-bulk.execute({ w: 0});
-bulk_c.execute({ w: 0});
+    if(counter % 500 === 0 ) {
+      bulk.execute({ w: 0});
+      bulk = db.empresa.initializeUnorderedBulkOp();
+      bulk_c.execute({ w: 0});
+      bulk_c = db.contrato.initializeUnorderedBulkOp();
+    }
+  });
+  print("insertando " + Object.keys(hashmap).length + " registros");
+  bulk.execute({ w: 0});
+  bulk_c.execute({ w: 0});
 
-counter=0;
-print("actualizando importes");
-bulk = db.empresa.initializeUnorderedBulkOp();
-db.empresa.find({}).forEach(function (doc) {
-  //nombres: hashmap_names[doc._id], 
-  bulk.find( { _id: doc._id } ).update( { $set: { importe_contrato: importe_hashmap[doc.proveedor_contratista], importe_contrato_usd: importe_hashmap_usd[doc.proveedor_contratista] } } );
-  counter++;
-  if(counter % 50000 === 0 ) {
-    print("ciclo " + counter);
-  }
-  if(counter % 500 === 0 ) {
-    bulk.execute({ w: 0});
-    bulk = db.empresa.initializeUnorderedBulkOp();
+  counter=0;
+  print("actualizando importes");
+  bulk = db.empresa.initializeUnorderedBulkOp();
+  db.empresa.find({}).forEach(function (doc) {
+    //nombres: hashmap_names[doc._id], 
+    bulk.find( { _id: doc._id } ).update( { $set: { importe_contrato: importe_hashmap[doc.proveedor_contratista], importe_contrato_usd: importe_hashmap_usd[doc.proveedor_contratista] } } );
+    counter++;
+    if(counter % 50000 === 0 ) {
+      print("ciclo " + counter);
+    }
+    if(counter % 500 === 0 ) {
+      bulk.execute({ w: 0});
+      bulk = db.empresa.initializeUnorderedBulkOp();
+    }
   }
 });
 bulk.execute({ w: 0});
--- scripts/mongo/10.updateEmpresas.js
+++ scripts/mongo/10.updateEmpresas.js
@@ -16,63 +16,65 @@ print('start update empresa');
 
 //agregamos la condición para buscar contratos sin, null funciona para cuando tiene el valor null o cuando no existe el campo
 db.contrato.find({ provedorContratista : null }).addOption(DBQuery.Option.noTimeout).forEach(function (doc){
-  nombre_sin_acentos = doc.proveedor_contratista.replace(preCompiledAccentsRegex, function(str,a,c,e,i,n,o,s,u,y,ae) { if(a) return 'a'; else if(c) return 'c'; else if(e) return 'e'; else if(i) return 'i'; else if(n) return 'n'; else if(o) return 'o'; else if(s) return 's'; else if(u) return 'u'; else if(y) return 'y'; else if(ae) return 'ae'; });
-  nombre = nombre_sin_acentos.toUpperCase();
-  nombre = nombre.replace(preCompiledComaRegex, "");
-  nombre = nombre.replace(preCompiledDotRegex, "");
-  nombre = nombre.replace(preCompiledSpaceRegex, "");
-  money = doc.importe_contrato;
-  if (isNaN(doc.importe_contrato)) {
-    money = parseFloat(doc.importe_contrato.replace(preCompiledMoneyRegex, ''));
-  }
-  if (doc.source == "https://sites.google.com/site/cnetuc/contratos_cnet_3") {
-    dateString = doc.fecha_inicio.match(preCompiledDateRegex);
-    date = new Date( dateString[3], dateString[2]-1, dateString[1] ).getFullYear();
-  } else {
-    date = new Date(doc.fecha_inicio).getFullYear();
-  }
+  if( doc.proveedor_contratista ){
+    nombre_sin_acentos = doc.proveedor_contratista.replace(preCompiledAccentsRegex, function(str,a,c,e,i,n,o,s,u,y,ae) { if(a) return 'a'; else if(c) return 'c'; else if(e) return 'e'; else if(i) return 'i'; else if(n) return 'n'; else if(o) return 'o'; else if(s) return 's'; else if(u) return 'u'; else if(y) return 'y'; else if(ae) return 'ae'; });
+    nombre = nombre_sin_acentos.toUpperCase();
+    nombre = nombre.replace(preCompiledComaRegex, "");
+    nombre = nombre.replace(preCompiledDotRegex, "");
+    nombre = nombre.replace(preCompiledSpaceRegex, "");
+    money = doc.importe_contrato;
+    if (isNaN(doc.importe_contrato)) {
+      money = parseFloat(doc.importe_contrato.replace(preCompiledMoneyRegex, ''));
+    }
+    if (doc.source == "https://sites.google.com/site/cnetuc/contratos_cnet_3") {
+      dateString = doc.fecha_inicio.match(preCompiledDateRegex);
+      date = new Date( dateString[3], dateString[2]-1, dateString[1] ).getFullYear();
+    } else {
+      date = new Date(doc.fecha_inicio).getFullYear();
+    }
 
-  //update or insert
-  var id = new ObjectId();
-  var obj = { _id: id, proveedor_contratista: nombre_sin_acentos, createdAt: today_date, updatedAt: today_date, slug : nombre, importe_contrato : 0, importe_contrato_usd : 0 };
-  bulk.find({ slug: nombre }).upsert().update({ $setOnInsert: obj });
-  if(doc.moneda === undefined || doc.moneda.toUpperCase() == "MXN")
-    bulk.find({ slug: nombre }).upsert().update({ $setOnInsert: { $inc: { importe_contrato : money } } });
-  else
-    bulk.find({ slug: nombre }).upsert().update({ $setOnInsert: { $inc: { importe_contrato_usd : money } } });
+    //update or insert
+    var id = new ObjectId();
+    var obj = { _id: id, proveedor_contratista: nombre_sin_acentos, createdAt: today_date, updatedAt: today_date, slug : nombre, importe_contrato : 0, importe_contrato_usd : 0 };
+    bulk.find({ slug: nombre }).upsert().update({ $setOnInsert: obj });
+    if(doc.moneda === undefined || doc.moneda.toUpperCase() == "MXN")
+      bulk.find({ slug: nombre }).upsert().update({ $setOnInsert: { $inc: { importe_contrato : money } } });
+    else
+      bulk.find({ slug: nombre }).upsert().update({ $setOnInsert: { $inc: { importe_contrato_usd : money } } });
 
-  //update contract
-  bulk_c.find({ _id: doc._id }).update({ $set: { codigoContratoProcedimiento : doc.codigo_contrato + '-' + doc.numero_procedimiento  ,fecha_inicio_year: date, origProvedorContratista: doc.proveedor_contratista, importe_contrato: money, slugProvedorContratista : nombre } });
-  
-  counter++;
-  if(counter % 50000 === 0 ) {
-    print("ciclo " + counter);
-  }
-  if(counter % 500 === 0 ) {
-    bulk.execute({ w: 0});
-    bulk = db.empresa.initializeUnorderedBulkOp();
-    bulk_c.execute({ w: 0});
-    bulk_c = db.contrato.initializeUnorderedBulkOp();
-  }
-});
-print("insertando registros");
-bulk.execute({ w: 0});
-bulk_c.execute({ w: 0});
+    //update contract
+    bulk_c.find({ _id: doc._id }).update({ $set: { codigoContratoProcedimiento : doc.codigo_contrato + '-' + doc.numero_procedimiento  ,fecha_inicio_year: date, origProvedorContratista: doc.proveedor_contratista, importe_contrato: money, slugProvedorContratista : nombre } });
+    
+    counter++;
+    if(counter % 50000 === 0 ) {
+      print("ciclo " + counter);
+    }
+    if(counter % 500 === 0 ) {
+      bulk.execute({ w: 0});
+      bulk = db.empresa.initializeUnorderedBulkOp();
+      bulk_c.execute({ w: 0});
+      bulk_c = db.contrato.initializeUnorderedBulkOp();
+    }
+  });
+  print("insertando registros");
+  bulk.execute({ w: 0});
+  bulk_c.execute({ w: 0});
 
 
-counter=0;
-print("actualizando contratos -> relación empresas");
-bulk_c = db.contrato.initializeUnorderedBulkOp();
-db.empresa.find({ }).addOption(DBQuery.Option.noTimeout).forEach(function (doc) {
-  //print(doc.slug); // , provedorContratista : null
-  bulk_c.find({ slugProvedorContratista : { $eq : doc.slug }, provedorContratista:null  }).update({ $set : {provedorContratista : doc._id} });//provedorContratista
-  counter++;
-  if(counter % 1500 === 0 ) {
-    print("ciclo " + counter);
-  }
-  if(counter % 100 === 0 ) {
-    bulk_c.execute({ w: 0});
-    bulk_c = db.contrato.initializeUnorderedBulkOp();
+  counter=0;
+  print("actualizando contratos -> relación empresas");
+  bulk_c = db.contrato.initializeUnorderedBulkOp();
+  db.empresa.find({ }).addOption(DBQuery.Option.noTimeout).forEach(function (doc) {
+    //print(doc.slug); // , provedorContratista : null
+    bulk_c.find({ slugProvedorContratista : { $eq : doc.slug }, provedorContratista:null  }).update({ $set : {provedorContratista : doc._id} });//provedorContratista
+    counter++;
+    if(counter % 1500 === 0 ) {
+      print("ciclo " + counter);
+    }
+    if(counter % 100 === 0 ) {
+      bulk_c.execute({ w: 0});
+      bulk_c = db.contrato.initializeUnorderedBulkOp();
+    }
   }
 });
 print('EXEC ' + counter);
