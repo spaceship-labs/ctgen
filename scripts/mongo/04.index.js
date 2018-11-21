5f52f7958892992285e1315b5b4b4fcb7b4a3f62
urielaero <aero.uriel@gmail.com>
2018-11-21 14:04:56 -0500

    fix index value DESC

--- scripts/mongo/04.index.js
+++ scripts/mongo/04.index.js
@@ -3,7 +3,7 @@ db.contrato.createIndex( { provedorContratista: 1 } );
 db.contrato.createIndex( { dependencia2: 1 } );
 db.contrato.createIndex( { unidadCompradora: 1 } );
 db.contrato.createIndex( { importe_contrato: 1 } );
-db.contrato.createIndex( { importe_contrato: 0 } );
+db.contrato.createIndex( { importe_contrato: -1 } );
 db.contrato.createIndex( { mondeda: 1 } );
 db.contrato.createIndex( { fecha_inicio_year: 1 } );
 
--- scripts/mongo/06.02.index.js
+++ scripts/mongo/06.02.index.js
@@ -1,9 +1,9 @@
 db.empresa.createIndex( { proveedor_contratista: "text" } );
 db.empresa.createIndex( { importe_contrato: 1 } );
-db.empresa.createIndex( { importe_contrato: 0 } );
+db.empresa.createIndex( { importe_contrato: -1 } );
 
 db.dependencia.createIndex( { siglas: 1 } );
-db.dependencia.createIndex( { siglas: 0 } );
-db.dependencia.createIndex( { dependencia: "text" } )
+db.dependencia.createIndex( { siglas: -1 } );
+db.dependencia.createIndex( { dependencia: "text" } );
 
-db.unidadcompradora.createIndex( { nombre_de_la_uc: "text" } )
+db.unidadcompradora.createIndex( { nombre_de_la_uc: "text" } );
